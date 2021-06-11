ec9887f2e9f1b9a51e3a585068325cf41be15d36
Bartosz Hejman <bartoszhejman@gmail.com>
2021-06-11 21:25:33 +0200

    Comment out freezing

--- RTCRtpReceiver.js
+++ RTCRtpReceiver.js
@@ -3,10 +3,10 @@ import MediaStreamTrack from './MediaStreamTrack';
 export default class RTCRtpReceiver {
     id: string;
     track: MediaStreamTrack;
-    
+
     constructor(id: string, track: MediaStreamTrack) {
         this.id = id;
         this.track = track;
-        Object.freeze(this);
+        // Object.freeze(this);
     }
 }
